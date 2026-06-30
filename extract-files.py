c4d25e6f41cbac3c99bf84fdd406981ca1e51902
Oliwier Krawczyk <36317711+TheMysticle@users.noreply.github.com>
2026-06-30 17:42:59 +0200

    sm8250-common: Auto-patch libcsextimpl.so for A16 compatibility

--- extract-files.py
+++ extract-files.py
@@ -130,6 +130,9 @@ blob_fixups: blob_fixups_user_type = {
         .sig_replace('F1 E9 D3 84 52 49 3F A0 72', 'F1 A9 00 80 52 09 00 A0 72'),
     'vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so': blob_fixup()
         .call(blob_fixup_nop_call, 'bl', '__cfi_check', '_ZN7android8hardware22configureRpcThreadpoolEmb@plt'),
+    'system_ext/lib64/libcsextimpl.so': blob_fixup()
+        .binary_regex_replace(b'_ZN7android17CameraThreadState13getCallingPidEv', b'getpid' + b'\x00' * 41)
+        .binary_regex_replace(b'_ZN7android17CameraThreadState13getCallingUidEv', b'getuid' + b'\x00' * 41),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
