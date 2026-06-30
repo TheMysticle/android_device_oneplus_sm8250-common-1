50fc56fc99657e64569c710d1f5669cf9144a687
Oliwier Krawczyk <36317711+TheMysticle@users.noreply.github.com>
2026-06-30 14:07:38 +0200

    sm8250-common: Add camera source dependencies
    These are required by the proprietary camera blobs but must be compiled from source. Added here to prevent them from being wiped out when extract-files.sh regenerates the vendor makefile.

--- common.mk
+++ common.mk
@@ -96,7 +96,11 @@ $(call soong_config_set_bool,QTI_GPT_UTILS,USE_BSG_FRAMEWORK,false)
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64
+    android.hardware.camera.provider@2.4-service_64 \
+    android.frameworks.cameraservice.common@2.0 \
+    android.frameworks.cameraservice.service@2.2 \
+    vendor.qti.hardware.camera.postproc@1.0.vendor \
+    android.hardware.camera.provider-V1-ndk
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
